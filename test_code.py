df8f4af1dd383a8d8a9c1c9e09dc9410705d3b76
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-08 09:44:25 +0200

    Add test_code.py

---
+++ test_code.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+./test_code.py; red_green_bar.py $? $COLUMNS
+red_green_bar.py is taken from https://github.com/kwadrat/rgb_tdd.git
+"""
+
+import sys
+import unittest
+
+
+class TestSimpleExample(unittest.TestCase):
+    def test_simple_example(self):
+        """
+        TestSimpleExample:
+        """
+        self.assertEqual(0, 1)
+
+fast_test_ls = [TestSimpleExample]
+
+
+def add_all_fast(suite):
+    for one_test in fast_test_ls:
+        suite.addTest(unittest.makeSuite(one_test))
+
+
+def summary_status(suite):
+    text_test_result = unittest.TextTestRunner().run(suite)
+    return not not (text_test_result.failures or text_test_result.errors)
+
+
+def perform_tests():
+    suite = unittest.TestSuite()
+    add_all_fast(suite)
+    return summary_status(suite)
+
+
+if __name__ == "__main__":
+    result = perform_tests()
+    sys.exit(result)

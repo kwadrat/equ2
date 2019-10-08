c6b34419f9e4c2bc8cccbf2f1611859b5f338997
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-08 10:09:05 +0200

    Add numbers

--- eis_nr.py
+++ eis_nr.py
@@ -15,6 +15,14 @@ class EisNumber:
             given_omega = 0
         self.part_omega = given_omega
 
+    def __add__(self, other):
+        '''
+        EisNumber:
+        '''
+        tmp_real = self.part_real + other.part_real
+        tmp_omega = self.part_omega + other.part_omega
+        return EisNumber(tmp_real, tmp_omega)
+
 class TestNumber(unittest.TestCase):
     def test_create_number(self):
         '''
@@ -31,3 +39,13 @@ class TestNumber(unittest.TestCase):
         obj = EisNumber(1, 2)
         self.assertEqual(obj.part_real, 1)
         self.assertEqual(obj.part_omega, 2)
+
+    def test_add_numbers(self):
+        '''
+        TestNumber:
+        '''
+        obj_a = EisNumber(10, 20)
+        obj_b = EisNumber(3, 4)
+        obj_c = obj_a + obj_b
+        self.assertEqual(obj_c.part_real, 13)
+        self.assertEqual(obj_c.part_omega, 24)

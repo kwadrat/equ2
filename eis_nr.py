acd35399cfc544ed2320641df7afc9577e9e73cd
Piotr Kasprzyk <ciri@ciri.pl>
2019-10-08 10:06:18 +0200

    Reorder to real, omega

--- eis_nr.py
+++ eis_nr.py
@@ -4,16 +4,16 @@
 import unittest
 
 class EisNumber:
-    def __init__(self, given_omega=None, given_real=None):
+    def __init__(self, given_real=None, given_omega=None):
         '''
         EisNumber:
         '''
-        if given_omega is None:
-            given_omega = 0
-        self.part_omega = given_omega
         if given_real is None:
             given_real = 0
         self.part_real = given_real
+        if given_omega is None:
+            given_omega = 0
+        self.part_omega = given_omega
 
 class TestNumber(unittest.TestCase):
     def test_create_number(self):
@@ -21,13 +21,13 @@ class TestNumber(unittest.TestCase):
         TestNumber:
         '''
         obj = EisNumber()
-        self.assertEqual(obj.part_omega, 0)
         self.assertEqual(obj.part_real, 0)
+        self.assertEqual(obj.part_omega, 0)
 
     def test_set_parts(self):
         '''
         TestNumber:
         '''
         obj = EisNumber(1, 2)
-        self.assertEqual(obj.part_omega, 1)
-        self.assertEqual(obj.part_real, 2)
+        self.assertEqual(obj.part_real, 1)
+        self.assertEqual(obj.part_omega, 2)
